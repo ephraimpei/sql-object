3fe1325e7ee66aa037a860097806827ce2660399
Ephraim Pei <ephdawg3374@gmail.com>
2015-12-07 22:00:44 -0500

    added searchable module.

--- lib/sql_object.rb
+++ lib/sql_object.rb
@@ -84,4 +84,38 @@ class SQLObject
     parse_all(results).first
   end
 
+  def insert
+    # drop 1 to avoid inserting id (the first column)
+    columns = self.class.columns.drop(1)
+    col_names = columns.map(&:to_s).join(", ")
+    question_marks = (["?"] * columns.count).join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1))
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
+  end
+
+  def update
+    set_line = self.class.columns
+      .map { |attr| "#{attr} = ?" }.join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        #{self.class.table_name}.id = ?
+    SQL
+  end
+
+  def save
+    id.nil? ? insert : update
+  end
+
 end
